89bfc19ab3e5d408c079bb93252b477bf2c64170
Do Van Duc Anh <dovanducanh06@gmail.com>
2024-07-14 07:45:47 +0700

    Add project files.

---
+++ eCom.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using eCom.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCom.Services.CouponAPI.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base() { }
+
+		public DbSet<Coupon> Coupons { get; set;}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+			{
+				IConfigurationRoot config = new ConfigurationBuilder()
+				   .SetBasePath(Directory.GetCurrentDirectory())
+				   .AddJsonFile("appsettings.json")
+				   .Build();
+				string connString = config.GetConnectionString("DefaultConnection");
+				optionsBuilder.UseSqlServer(connString); 
+			}
+		}
+	}
+}
