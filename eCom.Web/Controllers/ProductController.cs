2f2f1ff8a4345a07832f97bc3d1ad81945c99dea
Do Van Duc Anh <dovanducanh06@gmail.com>
2024-07-22 15:12:12 +0700

    done crud product

--- eCom.Web/Controllers/ProductController.cs
+++ eCom.Web/Controllers/ProductController.cs
@@ -55,14 +55,38 @@ namespace eCom.Web.Controllers
 			}
 			return View(productDTO);
 		}
-		public async Task<IActionResult> ProductEdit()
+		public async Task<IActionResult> ProductEdit(int productId)
 		{
-			return View();
+			ResponseDTO? response = await _productService.GetProductByIdAsync(productId);
+			if (response != null && response.IsSuccess)
+			{
+				ProductDTO? productDTO = JsonConvert.DeserializeObject<ProductDTO>(Convert.ToString(response.Result));
+
+				return View(productDTO);
+			}
+			else
+			{
+				TempData["error"] = response?.Message;
+			}
+			return NotFound();
 		}
 		[HttpPost]
 		public async Task<IActionResult> ProductEdit(ProductDTO productDTO)
 		{
-			return View();
+			if (ModelState.IsValid)
+			{
+				ResponseDTO? response = await _productService.UpdateProductAsync(productDTO);
+				if (response != null && response.IsSuccess)
+				{
+					TempData["success"] = "Product updated successfully";
+					return RedirectToAction(nameof(ProductIndex));
+				}
+				else
+				{
+					TempData["error"] = response?.Message;
+				}
+			}
+			return View(productDTO);
 		}
 		public async Task<IActionResult> ProductDelete(int productId)
 		{
