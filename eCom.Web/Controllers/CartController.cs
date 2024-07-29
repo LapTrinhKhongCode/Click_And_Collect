a8e1c9d88eaac9f9048147c6a8f3ba744cc68805
Do Van Duc Anh <dovanducanh06@gmail.com>
2024-07-29 16:24:25 +0700

    add item to cart

---
+++ eCom.Web/Controllers/CartController.cs
@@ -0,0 +1,43 @@
+﻿using eCom.Web.Models;
+using eCom.Web.Service.IService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace eCom.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+        public CartController(ICartService cartService) { 
+            _cartService = cartService;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> CartIndex()
+        {
+            return View( await LoadCartDTOBaseOnLoggedInUser());
+        }
+
+        public async Task<CartDTO> LoadCartDTOBaseOnLoggedInUser()
+        {
+            var userId = User.Claims.
+                Where(temp => temp.Type == JwtRegisteredClaimNames.Sub)?.
+                FirstOrDefault()?.Value;
+            ResponseDTO? responseDTO = await _cartService.GetCartByUserIdAsync(userId);
+            if (responseDTO != null && responseDTO.IsSuccess)
+            {
+
+                CartDTO cartDTO = JsonConvert.DeserializeObject<CartDTO>(Convert.ToString(responseDTO.Result));
+                return cartDTO;
+            }
+            return new CartDTO();
+        }
+
+
+
+
+
+    }
+}
--- eCom.Web/Controllers/HomeController.cs
+++ eCom.Web/Controllers/HomeController.cs
@@ -1,61 +1,106 @@
-using eCom.Services.ProductAPI.Models.DTO;
+
 using eCom.Web.Models;
 using eCom.Web.Service.IService;
+using IdentityModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace eCom.Web.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly IProductService _productService;
-		public HomeController(IProductService productService)
-		{
-			_productService = productService;
-		}
+    public class HomeController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+        public HomeController(IProductService productService, ICartService cartService)
+        {
+            _productService = productService;
+            _cartService = cartService;
+        }
 
-		public async Task<IActionResult> Index()
-		{
-			List<ProductDTO?> list = new();
-			ResponseDTO? response = await _productService.GetAllProductAsync();
-			if (response != null && response.IsSuccess)
-			{
-				list = JsonConvert.DeserializeObject<List<ProductDTO>>(Convert.ToString(response.Result));
-			}
-			else
-			{
-				TempData["error"] = response?.Message;
-			}
+        public async Task<IActionResult> Index()
+        {
+            List<ProductDTO?> list = new();
+            ResponseDTO? response = await _productService.GetAllProductAsync();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDTO>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
 
-			return View(list);
-		}
+            return View(list);
+        }
 
-		public async Task<IActionResult> ProductDetails(int productId)
-		{
-			ProductDTO? product = new();
-			ResponseDTO? response = await _productService.GetProductByIdAsync(productId);
-			if (response != null && response.IsSuccess)
-			{
-				product = JsonConvert.DeserializeObject<ProductDTO>(Convert.ToString(response.Result));
-			}
-			else
-			{
-				TempData["error"] = response?.Message;
-			}
+        public async Task<IActionResult> ProductDetails(int productId)
+        {
+            ProductDTO? product = new();
+            ResponseDTO? response = await _productService.GetProductByIdAsync(productId);
+            if (response != null && response.IsSuccess)
+            {
+                product = JsonConvert.DeserializeObject<ProductDTO>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
 
-			return View(product);
-		}
+            return View(product);
+        }
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+        [Authorize]
+        [HttpPost]
+        [ActionName("ProductDetails")]
+        public async Task<IActionResult> ProductDetails(ProductDTO productDTO)
+        {
+            CartDTO cartDTO = new CartDTO()
+            {
+                CartHeader = new CartHeaderDTO()
+                {
+                    UserId = User.Claims.Where(temp => temp.Type == JwtClaimTypes.Subject)?.First()?.Value
+                }
+            };
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+            CartDetailsDTO cartDetails = new CartDetailsDTO()
+            {
+                Count = productDTO.Count,
+                ProductId = productDTO.ProductId
+            };
+
+            List<CartDetailsDTO> cartDetailsDTOs = new List<CartDetailsDTO>()
+            {
+                cartDetails
+            };
+
+            cartDTO.CartDetails = cartDetailsDTOs;  
+
+            ResponseDTO? response = await _cartService.UpsertCartAsync(cartDTO);
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Item has been added to the Shopping Cart";
+                return RedirectToAction(nameof(Index));   
+            }
+            else
+            {
+                TempData["error"] = response?.Message;  
+            }
+            return View(productDTO);    
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
 }
