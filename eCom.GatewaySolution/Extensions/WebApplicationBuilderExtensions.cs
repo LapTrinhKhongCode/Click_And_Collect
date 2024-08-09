51c4e70662e7d3eba46fa2616b066722edb57bec
Do Van Duc Anh <dovanducanh06@gmail.com>
2024-08-09 17:15:03 +0700

    add ocelot

---
+++ eCom.GatewaySolution/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace eCom.GatewaySolution.Extensions
+{
+	public static class WebApplicationBuilderExtensions
+	{
+		public static WebApplicationBuilder AddAppAuthentication(this WebApplicationBuilder builder)
+		{
+
+			var secret = builder.Configuration.GetValue<string>("ApiSettings:Secret");
+			var issuer = builder.Configuration.GetValue<string>("ApiSettings:Issuer");
+			var audience = builder.Configuration.GetValue<string>("ApiSettings:Audience");
+
+			var key = Encoding.ASCII.GetBytes(secret);
+
+			builder.Services.AddAuthentication(temp =>
+			{
+				temp.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				temp.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+			}).AddJwtBearer(temp =>
+			{
+				temp.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = new SymmetricSecurityKey(key),
+					ValidateIssuer = true,
+					ValidIssuer = issuer,
+					ValidAudience = audience,
+					ValidateAudience = true
+				};
+			});
+			return builder;
+		}
+	}
+}
--- eCom.GatewaySolution/Program.cs
+++ eCom.GatewaySolution/Program.cs
@@ -1,6 +1,16 @@
+using eCom.GatewaySolution.Extensions;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Values;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.AddAppAuthentication();
+
+builder.Services.AddOcelot();
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.UseOcelot();
 app.Run();
